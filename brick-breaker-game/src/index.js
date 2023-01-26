033f24c3f081bdd1a1c3750a4631309e3bbf1afd
Iryna Abrosimova <iryna.abrosimova@cxdisruption.com>
2023-01-26 13:25:26 +0100

    feat: add paddle and key mooving of paddle

--- brick-breaker-game/src/index.js
+++ brick-breaker-game/src/index.js
@@ -1,8 +1,35 @@
+import Paddle from "./paddle.js";
+import { InputHandler } from "./input.js";
+
 const canvas = document.querySelector("#gameScreen");
 const ctx = canvas.getContext("2d");
 
-ctx.fillStyle = "#f00";
-ctx.fillRect(20, 20, 100, 100);
+const GAME_WIDTH = 800;
+const GAME_HIGHT = 600;
 
-ctx.fillStyle = "#ff0";
-ctx.fillRect(200, 200, 50, 50);
+ctx.clearRect(0, 0, 800, 600); //clean screen
+
+// ctx.fillStyle = "#f00"; // add color of object
+// ctx.fillRect(20, 20, 100, 100); // add object
+
+// ctx.fillStyle = "#ff0"; //change color for another object
+// ctx.fillRect(340, 300, 50, 50); //add second object
+
+let paddle = new Paddle(GAME_WIDTH, GAME_HIGHT);
+
+new InputHandler(paddle);
+
+let lastTime = 0;
+
+// move paddle
+function gameLoop(timestamp) {
+  let deltaTime = timestamp - lastTime; // how mush time has passed
+  lastTime = timestamp;
+
+  ctx.clearRect(0, 0, 800, 600);
+  paddle.update(deltaTime);
+  paddle.draw(ctx);
+
+  requestAnimationFrame(gameLoop);
+}
+gameLoop();
