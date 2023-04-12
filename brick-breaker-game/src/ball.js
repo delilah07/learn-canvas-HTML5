029b141b74268022e297d76b1278850cb4d90d53
Iryna Abrosimova <iryna.abrosimova@cxdisruption.com>
2023-04-12 13:38:21 +0200

    ball collision with paddle

--- brick-breaker-game/src/ball.js
+++ brick-breaker-game/src/ball.js
@@ -3,6 +3,8 @@ export default class Ball {
     this.image = document.querySelector('#image__ball');
     this.size = 20;
 
+    this.game = game;
+
     this.gameWidth = game.gameWidth;
     this.gameHeight = game.gameHeight;
     this.position = { x: 10, y: 10 };
@@ -22,11 +24,30 @@ export default class Ball {
     this.position.x += this.speed.x;
     this.position.y += this.speed.y;
 
+    //wall on left or right
     if (this.position.x + this.size > this.gameWidth || this.position.x < 0) {
       this.speed.x = -this.speed.x;
     }
+
+    //wall on top or bottom
     if (this.position.y + this.size > this.gameHeight || this.position.y < 0) {
       this.speed.y = -this.speed.y;
+    }
+
+    //check collision with paddle
+    const bottomOfBall = this.position.y + this.size;
+    const topOfPaddle = this.game.paddle.position.y;
+    const leftSiseOfPaddle = this.game.paddle.position.x;
+    const rightSiseOfPaddle =
+      this.game.paddle.position.x + this.game.paddle.width;
+
+    if (
+      bottomOfBall >= topOfPaddle &&
+      this.position.x >= leftSiseOfPaddle &&
+      this.position.x + this.size <= rightSiseOfPaddle
+    ) {
+      this.speed.y = -this.speed.y;
+      this.position.y = this.game.paddle.position.y - this.size;
     }
   }
 }
--- brick-breaker-game/src/game.js
+++ brick-breaker-game/src/game.js
@@ -9,20 +9,20 @@ export default class Game {
   }
   start() {
     this.paddle = new Paddle(this);
-
     new InputHandler(this.paddle);
 
     this.ball = new Ball(this);
+
+    this.gameObject = [this.ball, this.paddle];
   }
   update(deltaTime) {
-    this.paddle.update(deltaTime);
-
-    // ctx.drawImage(ball, 10, 10, 20, 20);
-    this.ball.update(deltaTime);
+    // this.paddle.update(deltaTime);
+    // this.ball.update(deltaTime);
+    this.gameObject.forEach((el) => el.update(deltaTime));
   }
   draw(ctx) {
-    this.paddle.draw(ctx);
-
-    this.ball.draw(ctx);
+    // this.paddle.draw(ctx);
+    // this.ball.draw(ctx);
+    this.gameObject.forEach((el) => el.draw(ctx));
   }
 }
